5f6ea5e0b15f7d0ca4582f18b70368bc4e5e4e99
Islam Ayman <islam.ayman.mohammed@gmail.com>
2025-11-23 11:02:12 +0200

    :bookmark: Bump version to 0.3.0 for upcoming release

--- src/pydantic_httpx/__init__.py
+++ src/pydantic_httpx/__init__.py
@@ -1,6 +1,6 @@
 """Integration library for HTTPX with Pydantic models."""
 
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 
 from pydantic_httpx.async_client import AsyncClient
 from pydantic_httpx.client import Client
